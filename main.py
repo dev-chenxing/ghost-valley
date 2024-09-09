39e2a5e25592b3a4edd1176173ac97d4a84f2bd9
陈刑 <chenxing.io@outlook.com>
2024-09-09 22:07:46 +0800

    cnlunar

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
+import threading
 from rich import print
 
+from core import timer
 from help.welcome import welcome
-from cmds import time
-
+# from cmds import time
 
 print(welcome)
-time.main()
+
+timer.game_time = timer.Time()
+timer.real_time = timer.Time()
+
+try:
+
+    running = True
+
+    game_time_thread = threading.Thread(
+        target=timer.game_time_thread, daemon=True)
+    real_time_thread = threading.Thread(
+        target=timer.real_time_thread, daemon=True)
+
+    game_time_thread.start()
+    real_time_thread.start()
+
+    while running:
+        cmd = input("> ")
+        if cmd == "time":
+            print(f"现在 游戏 时间是：[bright_yellow]{
+                timer.game_time.format()}[/bright_yellow]")
+
+except (KeyboardInterrupt, SystemExit):
+    print(":x: 退出")
